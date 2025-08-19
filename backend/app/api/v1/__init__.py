f1aa27e4be8304a3f78c0157a08bcabbe3d2970f
Varlachev Valery <valvarl@ya.ru>
2025-08-20 01:56:52 +0300

    collections router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -2,13 +2,15 @@
 
 from fastapi import APIRouter
 
-from .scraping import router as scraping_router
-from .products import router as products_router
+from .collections import router as collections_router
 from .debug import router as debug_router
 from .misc import router as misc_router
+from .products import router as products_router
+from .scraping import router as scraping_router
 
 router = APIRouter()
-router.include_router(misc_router)       # /, /search, /stats, ...
-router.include_router(scraping_router)   # /scrape/*
-router.include_router(products_router)   # /products/*
+router.include_router(collections_router)  # /collections/*
 router.include_router(debug_router)      # /debug/*
+router.include_router(misc_router)       # /, /search, /stats, ...
+router.include_router(products_router)   # /products/*
+router.include_router(scraping_router)   # /scrape/*
